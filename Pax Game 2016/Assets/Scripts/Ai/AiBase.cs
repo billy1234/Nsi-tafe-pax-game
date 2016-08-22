79d85cfe7d8f7c1d66d800487655463399fdc7c1
Billy Mihalarias <bmihalarias@gmail.com>
2016-08-22 16:18:08 +1000

    added ai framework, currently incomplete.

---
+++ Pax Game 2016/Assets/Scripts/Ai/AiBase.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public enum aiState
+{
+    PATROL, ATTACK, KITE_BACK
+};
+
+public class AiBase : MonoBehaviour {
+
+   
+
+    private readonly float unitTickRate = 1f;
+
+   // [HideInInspector]
+    public aiState state = aiState.PATROL;
+    public Transform target;
+
+    [Tooltip("The max range this unit will atack from before getting closer")]
+    public float maxRange =5f;
+
+    [Tooltip("set range to 0 if you do not with this unit to kite back")]
+    public float minRange =0f;
+
+    public voidEvent OnAiTick;
+
+	void Start ()
+    {
+        StartCoroutine(aiTick());
+	}
+
+    #if UNITY_EDITOR
+    void OnDrawGizmos()
+    {
+        if (Selection.activeGameObject == gameObject)
+        {
+            Gizmos.color = Color.green;
+
+            Gizmos.DrawWireSphere(transform.position, maxRange);
+
+
+            Gizmos.color = Color.red;
+
+            Gizmos.DrawWireSphere(transform.position, minRange);
+        }
+    }
+    #endif
+
+    IEnumerator aiTick()
+    {
+        while (gameObject.active)
+        {
+            if (target != null)
+            {
+                float targetDistance = Mathf.Abs((transform.position - target.position).magnitude);
+                if (targetDistance > maxRange)
+                {
+                    state = aiState.PATROL;
+                }
+                else if (targetDistance < minRange)
+                {
+                    state = aiState.KITE_BACK;
+                }
+                else
+                {
+                    state = aiState.ATTACK;
+                }
+
+            }
+            else
+            {
+                state = aiState.PATROL;
+            }
+            if (OnAiTick != null)
+            {
+                OnAiTick();
+            }
+
+            yield return new WaitForSeconds(unitTickRate);
+        }
+    }
+	
+
+
+}
---
+++ Pax Game 2016/Assets/Scripts/Ai/AiPathFinding.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(AiBase))]
+public class AiPathFinding : MonoBehaviour
+{
+
+    /// <summary>
+    /// all ai share the same patrol nodes
+    /// </summary>
+    public static Transform[] patrolNodes;
+    public Transform node;
+    private NavMeshAgent myNavMeshAgent;
+    private AiBase aiBase;
+    public float fleeSpeed =1f;
+    public void Awake()
+    {
+        aiBase = gameObject.GetComponent<AiBase>();
+        myNavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+
+    public void patrol()
+    {
+        if (node == null)
+        {
+            node = patrolNodes[Random.Range(0, patrolNodes.Length)];
+        }
+        else
+        {
+            myNavMeshAgent.SetDestination(node.position);
+        }
+    }
+
+    public void walkTowardTarget()
+    {
+        myNavMeshAgent.SetDestination(aiBase.target.position);
+    }
+
+    public void kiteBack()
+    {
+        myNavMeshAgent.ResetPath();
+        myNavMeshAgent.velocity = ((aiBase.target.position - transform.position).normalized * fleeSpeed);
+    }
+	
+}
